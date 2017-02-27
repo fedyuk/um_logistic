fb299faec6c466d67a1eb47d93056737b471c1c8
fedyukserhiy <fedyukserhiy@gmail.com>
2017-02-27 11:59:25 +0200

    example load base 64 string rom input in comments

--- UM_LOGISTIC_V1/Client/Controllers/Cooperation/CooperationApplicationController.js
+++ UM_LOGISTIC_V1/Client/Controllers/Cooperation/CooperationApplicationController.js
@@ -100,7 +100,17 @@
 			r.readAsBinaryString(f);
 		}
 		
-		//readAsArrayBuffer() for mobile
+	    //readAsArrayBuffer() for mobile
+		/*file = document.getElementById("coop-picture").files[0];
+		var reader = new FileReader();
+
+		reader.addEventListener("load", function () {
+		    preview.src = reader.result;
+		}, false);
+
+		if (file) {
+		    reader.readAsDataURL(file);
+		}*/
 	}
 	
 	// init
